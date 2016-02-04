c75012418b4f61d86e9e7c2b0543b2a93cd9839e
The space of Qratosone <qratosone@live.com>
2016-02-05 02:56:06 +0800

    1036 Done

--- 1036/1036/1036.cpp
+++ 1036/1036/1036.cpp
@@ -25,7 +25,48 @@ int main()
 	cin >> N;
 	vector<People>male;
 	vector<People>female;
-
+	People input;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> input.name >> input.sex >> input.subject >> input.score;
+		if (input.sex=="M")
+		{
+			male.push_back(input);
+		}
+		else
+		{
+			female.push_back(input);
+		}
+	}
+	sort(male.begin(), male.end(), cmp);
+	sort(female.begin(), female.end(), cmp);
+	if (!female.empty())
+	{
+		vector<People>::iterator iter = female.end() - 1;
+		cout << iter->name<< " " << iter->subject << endl;
+	}
+	else
+	{
+		cout << "Absent" << endl;
+	}
+	if (!male.empty())
+	{
+		vector<People>::iterator iter = male.begin();
+		cout << iter->name << " " << iter->subject << endl;
+	}
+	else
+	{
+		cout << "Absent" << endl;
+	}
+	if (male.size()!=0&&female.size()!=0)
+	{
+		int delta = (female.end()-1)->score - male.begin()->score;
+		cout << delta << endl;
+	}
+	else
+	{
+		cout << "NA" << endl;
+	}
     return 0;
 }
 
